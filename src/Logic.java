e06029a084dbbc74e68aa45c5702032a6b1dd4e4
Kuro <dev_kuro@yahoo.de>
2023-11-26 13:59:57 +0100

    refactor and added saving and setValue methods

--- src/Logic.java
+++ src/Logic.java
@@ -1,31 +1,30 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 
 public class Logic {
 
     private final Scanner scanner;
+    private final Map<String, Map<String, String>> sections = new HashMap<>();
+
     private String currentLine;
     private String sectionName;
-    private String key;
-    private String value;
-    private Map<String, Map<String, String>> sections = new HashMap<>();
 
     Logic(Scanner scanner) {
-    this.scanner = scanner;
+        this.scanner = scanner;
     }
-    public void readini(){
 
-        while(scanner.hasNextLine()){
+    public void readini() {
+        while (scanner.hasNextLine()) {
             currentLine = scanner.nextLine().trim();
-            System.out.println(currentLine);
-            if (currentLine.contains("#")){
+            if (currentLine.contains("#")) {
                 //IGNORE AND KEEP READING
-            }
-            else if(currentLine.contains("[")){
+            } else if (currentLine.contains("[")) {
                 ReadSection();
-            }
-            else if(!currentLine.contains("#") && !currentLine.contains("[") && !currentLine.isEmpty()){
+            } else if (!currentLine.contains("#") && !currentLine.contains("[") && !currentLine.isEmpty()) {
                 readSetting();
             }
 
@@ -33,50 +32,78 @@ public class Logic {
 
     }
 
-    public void ReadSection(){
+    public void ReadSection() {
         int index = currentLine.lastIndexOf("[");
-        sectionName = currentLine.substring(index +1, currentLine.length()-1);
+        sectionName = currentLine.substring(index + 1, currentLine.length() - 1);
         sections.put(sectionName, new HashMap<>());
     }
 
-    public void readSetting(){
+    public void readSetting() {
         int index = currentLine.lastIndexOf("=");
-        key = currentLine.substring(0,index-1);
-        value = currentLine.substring(index+1, currentLine.length());
+        String key = currentLine.substring(0, index - 1);
+        String value = currentLine.substring(index + 1).trim();
 
         Map<String, String> currentSection = sections.get(sectionName);
-        currentSection.put(key,value);
+        currentSection.put(key, value);
     }
 
-    public String getValue(String searchString){
+    //returns a String or null
+    public String getValue(String searchString) {
         int dotindex = searchString.lastIndexOf(".");
-            String sectionName = searchString.substring(0, dotindex);
-            String settingName = searchString.substring(dotindex+1, searchString.length());
+        String sectionName = searchString.substring(0, dotindex);
+        String settingName = searchString.substring(dotindex + 1);
 
-        if(sections.containsKey(sectionName)){
-            Map<String,String> outermap = sections.get(sectionName);
+        Map<String, String> innerMap = sections.get(sectionName);
 
-            if(outermap.containsKey(settingName)){
-
-                if(outermap.get(settingName).isEmpty()){
-                    System.out.println("Key: "+ settingName +"\nValue: null"); // just visual
-                    return value = null;
-                }
-                else{
-                    System.out.println("Key: "+ settingName +"\nValue:" + outermap.get(settingName)); // just visual
-                    value = outermap.get(settingName);
-                }
-
-            }
-
+        if (innerMap == null) {
+            return null;
         }
-
-        else{
-            System.out.println("Key does not exist");
-        }
-        return value;
+        //outer map exists
+        return innerMap.get(settingName);
 
     }
 
 
+    /**
+     * @param searchString The search string "section.setting"
+     * @param value        the value to put in the map
+     */
+    //setValue("blah.blih", "someValue")
+    public void setValue(String searchString, String value) {
+        int dotindex = searchString.lastIndexOf(".");
+        String sectionName = searchString.substring(0, dotindex);
+        String settingName = searchString.substring(dotindex + 1);
+
+        Map<String, String> innerMap = sections.get(sectionName);
+
+        if (innerMap == null) {
+            innerMap = new HashMap<>();
+            sections.put(sectionName, innerMap);
+        }
+        //innerMap will always exist
+        innerMap.put(settingName, value);
+    }
+
+    public void save(Path file) throws IOException {
+        //open output stream to write to a file
+        OutputStream out = Files.newOutputStream(file);
+        //convert OutputStream to PrintStream for convenience
+        PrintStream p = new PrintStream(out);
+        //Iterate through all the entries of the sections Map for the INI file.
+        for (Map.Entry<String, Map<String, String>> section : sections.entrySet()) {
+            String sectionName = section.getKey();
+            Map<String, String> settings = section.getValue();
+
+            //Write the section name in brackets to the file
+            p.println("[" + sectionName + "]");
+
+            //iterate through all the settings for this section
+            for (Map.Entry<String, String> setting : settings.entrySet()) {
+                //write them as key=value pairs to the ini file.
+                p.println(setting.getKey() + " = " + setting.getValue());
+            }
+            //add an empty line before starting with the next section in the iteration
+            p.println();
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
+    public static String out = "blah";
     // TODO
     // make it so it can handle any way the ini is written, basicly change how it reads them
 
 
     public static void main(String[] args) throws IOException {
-
         Path config = Path.of("./pcparts.ini");
 
         Scanner scanner = new Scanner(config);
@@ -17,11 +17,11 @@ public class Main {
         Logic logic = new Logic(scanner);
         logic.readini();
 
+        logic.save(config);
         System.out.println("--------------------------------");
         String value = logic.getValue("cables.cable2");
 
         System.out.println("-----\nStored value in the value string is: " + value); // just visual for making sure
 
     }
-
 }
