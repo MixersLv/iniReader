9b9a914cc23fe327b394b38be43af2913aebf5fd
BuildTools <unconfigured@null.spigotmc.org>
2023-11-26 12:58:38 +0200

    Refactor attempt

--- src/Logic.java
+++ src/Logic.java
@@ -1,80 +1,105 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Logic {
 
-    private final Scanner scanner;
-    private String currentLine;
-    private String sectionName;
-    private String key;
-    private String value;
-    private Map<String, Map<String, String>> sections = new HashMap<>();
+    //TODO
+    // possibly make it so each time a new line is read it resets all statuses
+    private String section = "";
+    private String setting = "";
+    private String key = "";
+    private Scanner scanner;
+    private String curentCharacter;
+    private int currentIndex;
+    boolean commentStatus = false;
+    boolean sectionStatus = false;
+    boolean settingStatus = false;
+    boolean keyStatus = false;
+    boolean sameSection = false;
 
-    Logic(Scanner scanner) {
-    this.scanner = scanner;
+    Logic(Scanner scanner){
+        this.scanner = scanner;
     }
-    public void readini(){
 
-        while(scanner.hasNextLine()){
-            currentLine = scanner.nextLine().trim();
-            System.out.println(currentLine);
-            if (currentLine.contains("#")){
-                //IGNORE AND KEEP READING
+    public void read() {
+
+        commentStatus = false;
+        sectionStatus = false;
+        settingStatus = false;
+        keyStatus = false;
+
+        while(scanner.hasNextLine()) {
+            String currentLine = scanner.nextLine().trim();
+
+            for (int i = 0; i < currentLine.length(); i++) {
+                currentIndex = i;
+                curentCharacter = String.valueOf(currentLine.charAt(i));
+
+                if (curentCharacter.equals("#")){
+                    commentStatus = true;
+                }
+                if(commentStatus == true){
+                    readComment();
+                }
+                //
+                if (sameSection == true){
+                    readSetting();
+                }
+                if(curentCharacter.equals("[")){
+                    sectionStatus = true;
+                    sameSection = true;
+                }
+                if (sectionStatus == true){
+                    readSection();
+                }
+                //
+                if (settingStatus == true){
+                    readSetting();
+                }
+                if (keyStatus == true){
+                    readKey();
+                }
+
+
             }
-            else if(currentLine.contains("[")){
-                ReadSection();
-            }
-            else if(!currentLine.contains("#") && !currentLine.contains("[") && !currentLine.isEmpty()){
-                readSetting();
-            }
+
+            System.out.println("SECTION: " + section);
+            System.out.println("SETTING: " + setting);
+            System.out.println("KEY: " + key);
+
+            System.out.println("--------NEXT LINE----------");
+            section = "";
+            setting = "";
+            key = "";
 
         }
-
     }
 
-    public void ReadSection(){
-        int index = currentLine.lastIndexOf("[");
-        sectionName = currentLine.substring(index +1, currentLine.length()-1);
-        sections.put(sectionName, new HashMap<>());
+    public void readComment(){
+        if (curentCharacter.equals("[")){
+            commentStatus = false;
+            sameSection =false;
+        }
+    }
+
+    public void readSection(){
+        if (curentCharacter.equals("]")){
+            sectionStatus = false;
+            settingStatus = true;
+            sameSection = true;
+        }
+        section = section+curentCharacter;
     }
 
     public void readSetting(){
-        int index = currentLine.lastIndexOf("=");
-        key = currentLine.substring(0,index-1);
-        value = currentLine.substring(index+1, currentLine.length());
-
-        Map<String, String> currentSection = sections.get(sectionName);
-        currentSection.put(key,value);
+        if (curentCharacter.equals("=")){
+            settingStatus = false;
+            keyStatus = true;
+        }
+        setting = setting + curentCharacter;
     }
 
-    public String getValue(String searchString){
-        int dotindex = searchString.lastIndexOf(".");
-            String sectionName = searchString.substring(0, dotindex);
-            String settingName = searchString.substring(dotindex+1, searchString.length());
-
-        if(sections.containsKey(sectionName)){
-            Map<String,String> outermap = sections.get(sectionName);
-
-            if(outermap.containsKey(settingName)){
-
-                if(outermap.get(settingName).isEmpty()){
-                    System.out.println("Key: "+ settingName +"\nValue: null"); // just visual
-                    return value = null;
-                }
-                else{
-                    System.out.println("Key: "+ settingName +"\nValue:" + outermap.get(settingName)); // just visual
-                    value = outermap.get(settingName);
-                }
-
-            }
-
-        }
-
-        else{
-            System.out.println("Key does not exist");
-        }
-        return value;
+    public void readKey(){
+        key = key + curentCharacter;
 
     }
 
